1e7377d4b4c0ed56c5511a501ecf489e0600a73f
Johannes Welsch <welschmoor@gmail.com>
2021-10-18 22:31:42 +0200

    efficient prime search

--- primes.py
+++ primes.py
@@ -1,20 +1,20 @@
 
 
-def isPrime(num):
-	for each in range(2, num):
-		if num%each == 0:
-			return False
-	return True
+def isprime(x):
+    primebool = False
+    for each in range(2, int(x**0.5)+1):
+
+        if x % each == 0:
+            return False
+    return True
 
 
-def createPrimeList(upToNum):
-	for each in range(2, upToNum):
-		if isPrime(each):
-			list_of_primes.append(each)
+numtocheck = 17
 
 
-list_of_primes = [];
-print(isPrime(7))
+primelist = []
+for num in range(1, 100, 2):
+    if isprime(num):
+        primelist.append(num)
 
-createPrimeList(100)
-print(list_of_primes)
+print(primelist)
